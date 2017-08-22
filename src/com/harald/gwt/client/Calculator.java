7a5c289faebd1279b561ea458ee3554dce45398a
Harald Billstein <harald.billstein@gmail.com>
2017-08-22 14:49:25 +0200

    Layout fixes and new functions added

--- src/com/harald/gwt/client/Calculator.java
+++ src/com/harald/gwt/client/Calculator.java
@@ -1,34 +1,81 @@
 package com.harald.gwt.client;
 
 public class Calculator {
+	
+	interface CalculatorListener {
+		public void result(String result);
+	}
+	
+	private CalculatorListener listener;
+	private double op1;
+	private double op2;
+	private String operator;
 
-	public String multiplication(double op1, double op2) {
-		double answer = op1 * op2;
-		return "" + answer;
+	private String multiplication(double op1, double op2) {
+		return "" + (op1 * op2);
 	}
 
-	public String modulo(double op1, double op2) {
-		double answer = op1 % op2;
-		return "" + answer;
+	private String modulo(double op1, double op2) {
+		return "" + (op1 % op2);
 	}
 
-	public String addition(double op1, double op2) {
-		double answer = op1 + op2;
-		return "" + answer;
+	private String addition(double op1, double op2) {
+		return "" + (op1 + op2);
 	}
 
-	public String subtraction(double op1, double op2) {
-		double answer = op1 - op2;
-		return "" + answer;
+	private String subtraction(double op1, double op2) {
+		return "" + (op1 - op2);
 	}
 
-	public String division(double op1, double op2) {
+	private String division(double op1, double op2) {
 		if (op2 == 0) {
 			return "Can´t devide with 0";
 		} else {
-			double answer = op1 / op2;
-			return "" + answer;
+			return "" + (op1 / op2);
 		}
+	}
+	
+	public void calculate() {
+		
+		String answer = null;
+		
+		switch (this.operator) {
+		case "*": 
+			answer = multiplication(this.op1, this.op2);
+			break;
+		case "%": 
+			answer = modulo(this.op1, this.op2);
+			break;
+		case "+": 
+			answer = addition(this.op1, this.op2);
+			break;
+		case "-": 
+			answer = subtraction(this.op1, this.op2);
+			break;
+		case "/": 
+			answer = division(this.op1, this.op2);
+			break;
+		default:
+			answer = "Not supported";
+			break;
+		}
+		
+		listener.result(this.op1 + this.operator +this.op2 + " = " + answer);
+		
+	}
+
+	public void setOps(double op1, double op2, String operator) {
+		this.op1 = op1;
+		this.op2 = op2;
+		this.operator = operator;
+	}
+
+	public void setListener(CalculatorListener listener) {
+		this.listener = listener;
+	}
+	
+	public void removeListener() {
+		this.listener = null;
 	}
 
 }
--- src/com/harald/gwt/client/Gwt_Calculator.java
+++ src/com/harald/gwt/client/Gwt_Calculator.java
@@ -2,6 +2,7 @@ package com.harald.gwt.client;
 
 import java.util.ArrayList;
 
+import com.gargoylesoftware.htmlunit.javascript.host.Event;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -12,23 +13,23 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.ValueBoxBase.TextAlignment;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.harald.gwt.client.Calculator.CalculatorListener;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 
-public class Gwt_Calculator implements EntryPoint {
+public class Gwt_Calculator implements EntryPoint, CalculatorListener {
 	private VerticalPanel mainPanel = new VerticalPanel();
 	private VerticalPanel resultPanel = new VerticalPanel();
 	private HorizontalPanel addPanel = new HorizontalPanel();
 	private TextBox operand1TextBox = new TextBox();
 	private TextBox operand2TextBox = new TextBox();
-	private Button calculateButton = new Button("Calculate");
-	private ListBox operatorListBox = new ListBox();
+	private TextBox operatorTextBox = new TextBox();
 	private FlexTable resultFlexTable = new FlexTable();
 	private Calculator calculator = new Calculator();
 	private FlexTable buttonsFlexitable = new FlexTable();
@@ -46,7 +47,7 @@ public class Gwt_Calculator implements EntryPoint {
 
 	private void calculate() {
 
-		final String operator = operatorListBox.getSelectedValue();
+		final String operator = operatorTextBox.getText();
 
 		if (!isDouble(operand1TextBox.getText().trim()) || !isDouble(operand2TextBox.getText().trim())) {
 			Window.alert("One of the operands is not an number");
@@ -56,41 +57,12 @@ public class Gwt_Calculator implements EntryPoint {
 		double operand1 = Double.parseDouble(operand1TextBox.getText().trim());
 		double operand2 = Double.parseDouble(operand2TextBox.getText().trim());
 
-		if (operator.equals("*")) {
-			String answer = calculator.multiplication(operand1, operand2);
-			Window.alert("The answer is: " + answer);
-			addToResultTable("" + operand1 + "*" + operand2, "  =  " + answer);
-			resetCalculator();
-		}
+		calculator.setListener(this);
+		calculator.setOps(operand1, operand2, operator);
+		calculator.calculate();
 
-		else if (operator.equals("%")) {
-			String answer = calculator.modulo(operand1, operand2);
-			Window.alert("The answer is: " + answer);
-			addToResultTable("" + operand1 + "%" + operand2, "  =  " + answer);
-			resetCalculator();
-		}
+		resetCalculator();
 
-		else if (operator.equals("+")) {
-			String answer = calculator.addition(operand1, operand2);
-			Window.alert("The answer is: " + answer);
-			addToResultTable("" + operand1 + "+" + operand2, "  =  " + answer);
-			resetCalculator();
-		}
-
-		else if (operator.equals("-")) {
-			String answer = calculator.subtraction(operand1, operand2);
-			Window.alert("The answer is: " + answer);
-			addToResultTable("" + operand1 + "-" + operand2, "  =  " + answer);
-			resetCalculator();
-
-		} else if (operator.equals("/")) {
-			String answer = calculator.division(operand1, operand2);
-			if (isDouble(answer)) {
-				addToResultTable("" + operand1 + "/" + operand2, "  =  " + answer);
-				resetCalculator();
-			}
-			Window.alert("The answer is: " + answer);
-		}
 	}
 
 	// Checks if a String could be seen as an double
@@ -105,34 +77,90 @@ public class Gwt_Calculator implements EntryPoint {
 
 	private void setUpLayout() {
 
-		operatorListBox.addItem("*");
-		operatorListBox.addItem("%");
-		operatorListBox.addItem("+");
-		operatorListBox.addItem("-");
-		operatorListBox.addItem("/");
-
-		resultFlexTable.setText(0, 0, "History");
+		operand1TextBox.setPixelSize(140, 15);
+		operand2TextBox.setPixelSize(140, 15);
+		operatorTextBox.setPixelSize(10, 15);
+		operatorTextBox.setAlignment(TextAlignment.CENTER);
 
 		// Setting up keypad
 		int buttonName = 9;
-		for (int i = 0; i <= 2; i++) {
+		for (int i = 1; i <= 3; i++) {
 			for (int j = 2; j >= 0; j--) {
 				Button button = new Button("" + buttonName);
-				button.setPixelSize(50, 50);
+				button.setPixelSize(75, 75);
 				buttonsFlexitable.setWidget(i, j, button);
 				buttons.add(button);
 				buttonName--;
 			}
 		}
+
+		// TODO im sorry, need to loop this :(
 		Button button0 = new Button("" + buttonName);
-		button0.setPixelSize(50, 50);
-		buttonsFlexitable.setWidget(3, 0, button0);
-		Button comma = new Button("" + ".");
-		comma.setPixelSize(50, 50);
-		buttonsFlexitable.setWidget(3, 1, comma);
-		Button backButton = new Button("del");
-		backButton.setPixelSize(50, 50);
-		buttonsFlexitable.setWidget(3, 2, backButton);
+		button0.setPixelSize(75, 75);
+		buttonsFlexitable.setWidget(4, 0, button0);
+
+		Button button00 = new Button("00");
+		button00.setPixelSize(75, 75);
+		buttonsFlexitable.setWidget(4, 1, button00);
+
+		Button comma = new Button(".");
+		comma.setPixelSize(75, 75);
+		buttonsFlexitable.setWidget(4, 2, comma);
+
+		Button backButton = new Button("C");
+		backButton.setStyleName("clearbuttons");
+		backButton.setPixelSize(75, 75);
+		buttonsFlexitable.setWidget(0, 0, backButton);
+
+		Button clearButton = new Button("AC");
+		clearButton.setStyleName("clearbuttons");
+		clearButton.setPixelSize(75, 75);
+		buttonsFlexitable.setWidget(0, 1, clearButton);
+
+		Button multiplicationButton = new Button("*");
+		multiplicationButton.setTitle("Operator");
+		multiplicationButton.setStyleName("operators");
+		multiplicationButton.setPixelSize(75, 75);
+		buttonsFlexitable.setWidget(1, 3, multiplicationButton);
+
+		Button moduloButton = new Button("%");
+		moduloButton.setTitle("Operator");
+		moduloButton.setStyleName("operators");
+		moduloButton.setPixelSize(75, 75);
+		buttonsFlexitable.setWidget(0, 2, moduloButton);
+
+		Button additionButton = new Button("+");
+		additionButton.setTitle("Operator");
+		additionButton.setStyleName("operators");
+		additionButton.setPixelSize(75, 75);
+		buttonsFlexitable.setWidget(3, 3, additionButton);
+
+		Button subtractionButton = new Button("-");
+		subtractionButton.setTitle("Operator");
+		subtractionButton.setStyleName("operators");
+		subtractionButton.setPixelSize(75, 75);
+		buttonsFlexitable.setWidget(2, 3, subtractionButton);
+
+		Button divisionButton = new Button("/");
+		divisionButton.setTitle("Operator");
+		divisionButton.setStyleName("operators");
+		divisionButton.setPixelSize(75, 75);
+		buttonsFlexitable.setWidget(0, 3, divisionButton);
+
+		Button calculateButton = new Button("=");
+		calculateButton.setTitle("Calculate");
+		calculateButton.setStyleName("Equal");
+		calculateButton.setPixelSize(75, 75);
+		buttonsFlexitable.setWidget(4, 3, calculateButton);
+
+		buttons.add(button00);
+		buttons.add(clearButton);
+		buttons.add(calculateButton);
+		buttons.add(multiplicationButton);
+		buttons.add(moduloButton);
+		buttons.add(additionButton);
+		buttons.add(subtractionButton);
+		buttons.add(divisionButton);
 
 		buttons.add(button0);
 		buttons.add(comma);
@@ -141,9 +169,8 @@ public class Gwt_Calculator implements EntryPoint {
 		resultPanel.add(resultFlexTable);
 
 		addPanel.add(operand1TextBox);
-		addPanel.add(operatorListBox);
+		addPanel.add(operatorTextBox);
 		addPanel.add(operand2TextBox);
-		addPanel.add(calculateButton);
 
 		mainPanel.add(resultPanel);
 		mainPanel.add(addPanel);
@@ -154,19 +181,28 @@ public class Gwt_Calculator implements EntryPoint {
 
 	private void setUpListeners() {
 
-		calculateButton.addClickHandler(new ClickHandler() {
-
-			public void onClick(ClickEvent event) {
-				calculate();
-			}
-		});
-
 		operand1TextBox.addKeyDownHandler(new KeyDownHandler() {
 
 			@Override
 			public void onKeyDown(KeyDownEvent event) {
 				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					calculate();
+
+					String temp = operand1TextBox.getText().substring((operand1TextBox.getText().length() - 1));
+
+					if (temp.equals("/") || temp.equals("*") || temp.equals("+") || temp.equals("-") || temp.equals("%")
+							|| temp.equals("=")) {
+						removeLastDigit();
+					}
+
+					if (operatorTextBox.getText().isEmpty()) {
+						operatorTextBox.setFocus(true);
+					} else if (operand2TextBox.getText().isEmpty()) {
+						operand2TextBox.setFocus(true);
+						isoperand1TextBoxSelected = false;
+					} else {
+						calculate();
+					}
+
 				}
 
 			}
@@ -186,7 +222,22 @@ public class Gwt_Calculator implements EntryPoint {
 			@Override
 			public void onKeyDown(KeyDownEvent event) {
 				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					calculate();
+					
+					String temp = operand2TextBox.getText().substring((operand1TextBox.getText().length() - 1));
+
+					if (temp.equals("/") || temp.equals("*") || temp.equals("+") || temp.equals("-") || temp.equals("%")
+							|| temp.equals("=")) {
+						removeLastDigit();
+					}
+
+					if (operatorTextBox.getText().isEmpty()) {
+						operatorTextBox.setFocus(true);
+					} else if (operand1TextBox.getText().isEmpty()) {
+						operand1TextBox.setFocus(true);
+						isoperand1TextBoxSelected = true;
+					} else {
+						calculate();
+					}
 				}
 
 			}
@@ -204,14 +255,53 @@ public class Gwt_Calculator implements EntryPoint {
 			buttons.get(i).addClickHandler(new ClickHandler() {
 
 				public void onClick(ClickEvent event) {
-					if (((Button) event.getSource()).getText().equals("del")) {
+					if (((Button) event.getSource()).getText().equals("C")) {
 						removeLastDigit();
-					} else {
+					} else if (((Button) event.getSource()).getTitle().equalsIgnoreCase("Operator")) {
+
+						if (isoperand1TextBoxSelected) {
+							isoperand1TextBoxSelected = false;
+						} else {
+							isoperand1TextBoxSelected = true;
+						}
+
+						operatorPressed(((Button) event.getSource()).getText());
+
+					} else if (((Button) event.getSource()).getTitle().equalsIgnoreCase("Calculate")) {
+						calculate();
+					} else if (((Button) event.getSource()).getText().equals("AC")) {
+						resetCalculator();
+					}
+
+					else {
 						numPadPressed(((Button) event.getSource()).getText());
 					}
 				}
 			});
 		}
+
+		operatorTextBox.addKeyDownHandler(new KeyDownHandler() {
+
+			@Override
+			public void onKeyDown(KeyDownEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					if (operand1TextBox.getText().isEmpty()) {
+						isoperand1TextBoxSelected = true;
+						operand1TextBox.setFocus(true);
+					} else if (operand2TextBox.getText().isEmpty()) {
+						isoperand1TextBoxSelected = false;
+						operand2TextBox.setFocus(true);
+					} else {
+						calculate();
+					}
+				}
+			}
+		});
+
+	}
+
+	private void operatorPressed(String text) {
+		operatorTextBox.setText(text);
 	}
 
 	private void numPadPressed(String keyPressed) {
@@ -225,12 +315,17 @@ public class Gwt_Calculator implements EntryPoint {
 	private void resetCalculator() {
 		operand1TextBox.setText("");
 		operand2TextBox.setText("");
+		operatorTextBox.setText("");
 	}
 
-	private void addToResultTable(String question, String answer) {
+	private void addToResultTable(String history) {
+
+		if (resultFlexTable.getRowCount() == 0) {
+			resultFlexTable.setText(0, 0, "History");
+		}
+
 		int row = resultFlexTable.getRowCount();
-		resultFlexTable.setText(row, 1, question);
-		resultFlexTable.setText(row, 2, answer);
+		resultFlexTable.setText(row, 1, history);
 	}
 
 	private void removeLastDigit() {
@@ -240,6 +335,12 @@ public class Gwt_Calculator implements EntryPoint {
 		} else {
 			operand2TextBox.setText(operand2TextBox.getText().substring(0, operand2TextBox.getText().length() - 1));
 		}
+	}
+
+	@Override
+	public void result(String result) {
+		Window.alert(result);
+		addToResultTable(result);
 	}
 
 }
